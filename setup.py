bc3361cbbc46b55af0fe3d39d2f405dfe6f49721
legalnlp21 <88805344+legalnlp21@users.noreply.github.com>
2021-08-12 16:39:25 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,5 +19,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['ftfy']
+    install_requires=['ftfy','wget']
 ) 
