48152f52603a6219e83c030d50a054418960a36f
legalnlp21 <88805344+legalnlp21@users.noreply.github.com>
2021-08-14 16:20:49 -0300

    Update get_premodel.py

--- legalnlp/get_premodel.py
+++ legalnlp/get_premodel.py
@@ -12,17 +12,17 @@ def get_premodel(model):
             d = ''
         with zipfile.ZipFile(d+filename, "r") as zip_ref:
             zip_ref.extractall(d+filename.replace('.zip', ''))
-    # W2V
-    if model == 'word':
+    # Download Word2Vec Continuous Bag-of-Words (CBOW) or Doc2Vec Distributed Memory (DM)
+    if model == 'wdocdm':
         url2 = 'https://ndownloader.figshare.com/files/29110809'
         filename2 = wget.download(url2, out=d)
         if d == None:
             d = ''
         with zipfile.ZipFile(d+filename2, "r") as zip_ref:
             zip_ref.extractall(d+filename2.replace('.zip', ''))
-    # D2V
-    if model == 'doc':
-        url2 = 'https://ndownloader.figshare.com/files/29110809'
+    # Download Doc2Vec Distributed Bag-of-Words (DBOW) or Word2Vec Skip-Gram (SG)
+    if model == 'wdocdbow':
+        url2 = 'https://ndownloader.figshare.com/files/29138835'
         filename2 = wget.download(url2, out=d)
         if d == None:
             d = ''
