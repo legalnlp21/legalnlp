79eb9f5b52f9a9411471b45a93becb0b2403288c
legalnlp21 <88805344+legalnlp21@users.noreply.github.com>
2021-08-26 20:01:11 -0300

    Update get_premodel.py

--- legalnlp/get_premodel.py
+++ legalnlp/get_premodel.py
@@ -6,23 +6,30 @@ def get_premodel(model):
     d = None
     if model == 'bert':
         # BERTikal
-        url = 'https://ndownloader.figshare.com/files/29110152'
+        url = 'https://ndownloader.figshare.com/files/30446754'
         filename = wget.download(url, out=d)
         if d == None:
             d = ''
         with zipfile.ZipFile(d+filename, "r") as zip_ref:
             zip_ref.extractall(d+filename.replace('.zip', ''))
-    # Download files to use Word2Vec Continuous Bag-of-Words (CBOW) or Doc2Vec Distributed Memory (DM)
-    if model == 'wdocdm':
-        url2 = 'https://ndownloader.figshare.com/files/29110809'
+    # Download files to use in Word2Vec and Doc2Vec
+    if model == 'wodc':
+        url2 = 'https://ndownloader.figshare.com/files/30446736'
         filename2 = wget.download(url2, out=d)
         if d == None:
             d = ''
         with zipfile.ZipFile(d+filename2, "r") as zip_ref:
             zip_ref.extractall(d+filename2.replace('.zip', ''))
     # Download files to use Doc2Vec Distributed Bag-of-Words (DBOW) or Word2Vec Skip-Gram (SG)
-    if model == 'wdocdbow':
-        url2 = 'https://ndownloader.figshare.com/files/29138835'
+    if model == 'phraser':
+        url2 = 'https://ndownloader.figshare.com/files/30446727'
+        filename2 = wget.download(url2, out=d)
+        if d == None:
+            d = ''
+        with zipfile.ZipFile(d+filename2, "r") as zip_ref:
+            zip_ref.extractall(d+filename2.replace('.zip', ''))
+    if model == 'fasttext':
+        url2 = 'https://ndownloader.figshare.com/files/30446739'
         filename2 = wget.download(url2, out=d)
         if d == None:
             d = ''
